3ee769ab47de00cc7ec043351f91f9a744c90ae3
Tumaini Wekesa <tumainiwamukota@gmail.com>
2025-04-29 06:16:40 +0300

    Added challenge 4 on importing and using the scanner

---
+++ Challange4.java
@@ -0,0 +1,26 @@
+//Challange 4 was on importing and using the scanner
+
+import java.util.Scanner;
+
+public class Challange4 {
+    Scanner s1 = new Scanner(System.in);
+
+    public void readNameAndAge(){
+        String name;
+        int age;
+        System.out.print("Name? ");
+        name = s1.next();
+
+        System.out.print("Age? ");
+        age = s1.nextInt();
+
+        System.out.println("Hello "+name+". You're "+age+" springs yonug");
+
+        
+    }
+    public static void main(String[] args) {
+        Challange4 c4 = new Challange4();
+
+        c4.readNameAndAge();
+    }
+}
