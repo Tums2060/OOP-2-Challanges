ff6f979aa394ce782bc4c97d0e396a709353ab0d
Tumaini Wekesa <tumainiwamukota@gmail.com>
2025-04-29 05:46:13 +0300

    Updated the Challange 1

---
+++ Challange1.java
@@ -0,0 +1,20 @@
+//Challange 1 Involved Using class bodies
+//Below is the implementation
+
+//Here i have made the class
+public class Challange1{
+
+    //created a function
+    void greet(){
+        
+        System.out.println("Hi there");
+    }
+
+    public static void main(String [] args){
+        //creating an object of the class
+        Challange1 c1 = new Challange1();
+
+        //implementing the function with the object
+        c1.greet();
+    }
+}
