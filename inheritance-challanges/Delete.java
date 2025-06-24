242dacc9ea9c6ae920d3ba77e7bb73fad47b63ad
Tumaini Wekesa <tumainiwamukota@gmail.com>
2025-06-24 10:01:53 +0300

    Inheritance Challanges

---
+++ inheritance-challanges/Delete.java
@@ -0,0 +1,7 @@
+public class Delete implements DataSource{
+
+    public void execute(){
+        System.out.println("Data has been deleted");
+    }
+    
+}
---
+++ inheritance-challanges/Update.java
@@ -0,0 +1,5 @@
+public class Update implements DataSource{
+    public void execute(){
+        System.out.println("Data had been Updated Successfully");
+    }
+}
