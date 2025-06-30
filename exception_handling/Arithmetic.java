19faaf6fedbbaa9208dd0c01adeb56400be834f4
Tumaini Wekesa <tumainiwamukota@gmail.com>
2025-06-30 15:53:56 +0300

    Exception Handling in Java

---
+++ exception_handling/Arithmetic.java
@@ -0,0 +1,14 @@
+package exception_handling;
+
+
+import java.io.*;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        int number1 = 100;
+        int number2 = 0;
+        int answer = number1/number2;
+
+        System.out.println("Answer:"+answer);
+    }
+}
---
+++ exception_handling/ArithmeticJava.java
@@ -0,0 +1,21 @@
+package exception_handling;
+
+import java.io.*;
+
+public class ArithmeticJava {
+    public static void main(String[] args) {
+        int number1 = 300;
+        int number2 = 0;
+
+        try{
+            int answer = number1/number2;
+            System.out.println("Answer:"+answer);
+        }
+        catch(ArithmeticException e){
+            System.out.println("Error: Division by zero is not allowed");
+        }
+        finally{
+            System.out.println("Program continues after handling exception");
+        }
+    }
+}
